d9408439ef82b6a93eae1bf18ff86078e451225f
TaffyIrina <96007474+TaffyIrina@users.noreply.github.com>
2021-12-28 01:37:42 +0300

    Add files via upload

---
+++ Lecture 2/Task_2.1.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def translate (input_str):
+    morse = { 'a': '*—', 'b': '—***', 'c': '—*—*',
+          'd': '—**', 'e': '*', 'f': '**—*',
+          'g': '——*', 'h': '****', 'i': '**',
+          'j': '*———', 'k': '—*—', 'l': '*—**',
+          'm': '——', 'n': '—*', 'o': '———',
+          'p': '*——*', 'q': '——*—', 'r': '*—*',
+          's': '***', 't': '—', 'u': '**—',
+          'v': '***—', 'w': '*——', 'x': '—**—',
+          'y': '—*——', 'z': '——**'}
+    input_str.lower()
+    trans =list(input_str)
+    end_str = ''
+    for i in range(len(trans)):
+        end_str = end_str + morse.get(trans[i],9)
+    print(end_str)
+            
+
+
+def main(input_string):
+    print ('Введите строку, которую хотите перевести')
+    input_str = input()
+    translate (input_str)
+
+
+if __name__ == '__main__':
+    main(sys.argv[0])
